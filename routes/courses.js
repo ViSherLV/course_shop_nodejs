8a6b1bcde73c49743fe5f787a0648ee6a872a710
VisherLV <visherlv@gmail.com>
2020-05-01 23:44:03 +0300

    start project

---
+++ routes/courses.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const router = Router();
+const Course = require("../models/course");
+
+router.get("/", async (req, res) => {
+  const courses = await Course.getAll();
+  res.render("courses", {
+    title: "Курсы",
+    isCourses: true,
+    courses,
+  });
+});
+
+module.exports = router;
