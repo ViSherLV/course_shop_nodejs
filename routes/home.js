e7fe09a01859205d1ce000d44bd21a3f07a2e166
Your Name <vishersb@gmail.com>
2020-10-26 15:03:11 +0200

    fix

--- routes/home.js
+++ routes/home.js
@@ -4,92 +4,76 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const keys = require('../keys');
 
-// const localizationsSchema = new Schema({
-//   name: String
-// });
-
-const languagesSchema = new Schema({
-  code: String,
-  text: String,
-});
-
-const regionSchema = new Schema({
-  code: String,
-  text: String,
-});
-
-const localizationsSchema = new Schema({
-  languages: [languagesSchema],
-  regionCode: [regionSchema],
-});
 
 router.get("/", async (req, res) => {
-try{
-  const testDB = await mongoose.createConnection('mongodb+srv://Rake_System_TEST:Aj01D1dG6XPXdAIY@test-swnqr.gcp.mongodb.net/Rake-System-TEST', {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-  });
-  const uatDB = await mongoose.createConnection('mongodb+srv://Rake_System_UAT:HWAaE9yb6SWMgppY@uat-2arol.gcp.mongodb.net/Rake-System-PROD', {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-  });
+  try {
+    const languagesSchema = new Schema({
+      code: String,
+      text: String,
+    });
 
-  let localizationUAT = uatDB.model('localizations', localizationsSchema);
-  const localizationTEST = testDB.model('localizations', localizationsSchema);
-  const localization1 = testDB.model('localizations1', localizationsSchema)
-  const local_test_back = testDB.model('localizations_26.10.2020',localizationsSchema )
-  const uatResult = await localizationUAT.find({})
-  const testResult = await localizationTEST.find({});
-  
+    const regionSchema = new Schema({
+      code: String,
+      text: String,
+    });
 
- //const unique= uatResult.concat(testResult.filter(({_id}) => !ids.includes(_id)))
- const unique = uatResult.concat(testResult);
+    const schema = new Schema({
+      languages: [languagesSchema],
+      regionCode: [regionSchema],
+    });
 
-//  let unique = result.filter(function(elem, index, self) {
-//   return index === self.indexOf(elem);
-// })
+    const OUTPUT_SERVER_URL = 'mongodb+srv://Rake_System_TEST:Aj01D1dG6XPXdAIY@test-swnqr.gcp.mongodb.net/Rake-System-TEST';
+    const INPUT_SERVER_URL = 'mongodb+srv://Rake_System_UAT:HWAaE9yb6SWMgppY@uat-2arol.gcp.mongodb.net/Rake-System-PROD';
+    const COLLECTION_NAME = 'localizations'
+
+    const inputServer = await mongoose.createConnection(OUTPUT_SERVER_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
+    const outputServer = await mongoose.createConnection(INPUT_SERVER_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
+
+    let outDB = outputServer.model(COLLECTION_NAME, schema);
+    const inDB = inputServer.model(COLLECTION_NAME, schema);
+    const outTable = await outDB.find({})
+    const inTable = await inDB.find({});
+
+    const allFields = outTable.concat(inTable);
 
 
-var used = {};
-var filtered = unique.filter(function(obj) {
-    return obj._id in used ? 0:(used[obj._id]=1);
-});
-const ids = testResult.map(item=>item._id.toString());
-console.log(`ids`, ids)
-filtered.forEach(function(item){
+    function getUniqueFields(allFields) {
+      var used = {};
 
-})
-  // var elements = unique.reduce(function(previous, current) {
+      var filtered = allFields.filter(field => {
+        return field._id in used ? 0 : (used[field._id] = 1);
+      });
+      const existingRecords = inTable.map(item => item._id.toString());
 
-  //   var object = previous.filter(object => object.timestamp === current.timestamp);
-  //   if (object.length == 0) {
-  //     previous.push(current);
-  //   }
-  //   return previous;
-  // }, []);
-  // localizationTEST.insertMany(filtered, function(err, result){
-  //   if (err) {
-  //     console.log(err);
-  //   } else {
-  //     console.log(result);
-  //   }
-  // })
-  console.log(`filtered,`, filtered)
-const id = '5e9d65f83b27d53ae85df928';
-  const filtered2 = filtered.filter(function(item,index,array){
-       if(ids.includes(item._id.toString())){
-        return false 
-       }
-       return true
- 
-  })
-  console.log('uatDBlength', uatResult.length)
-  console.log('testDBlenth', testResult.length)
-  res.send(filtered2).status(200)
-}catch(e){
+      const recordsForWrite = filtered.filter(item => {
+        if (existingRecords.includes(item._id.toString())) {
+          return false
+        }
+        return true
 
-throw new Error(e)
-}
+      })
+      return recordsForWrite;
+
+    }
+    const uniqueFields = getUniqueFields(allFields)
+
+    localizationTEST.insertMany(uniqueFields, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result);
+      }
+    })
+    res.send(uniqueFields).status(200)
+  } catch (e) {
+    throw new Error(e)
+  }
 
   // res.render("index", {
   //   title: "Main page",
